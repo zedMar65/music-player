2eae3f233005d4ca505f9222777db8c25b9f0781
pijus <pijuspie@gmail.com>
2025-01-08 19:11:35 +0200

    Added play-pause button, speed control, fixed note pitch.

--- player.js
+++ player.js
@@ -1,12 +1,11 @@
-let speed = 400;
-
 function getFreq(freq) {
-    return Tone.Frequency("C4").harmonize([freq - 1])[0];
+    return Tone.Frequency("C4").harmonize([16 - freq])[0];
 }
 
 function getNotes() {
     const notes = [];
     const displayWindowX = document.querySelector(".display").getBoundingClientRect().left;
+    const speed = document.getElementById("speed").value;
 
     const noteElements = document.querySelectorAll(".note");
     noteElements.forEach(note => {
@@ -21,15 +20,37 @@ function getNotes() {
 }
 
 const activeSynth = [];
+let playTimeout;
 
 function playNotes() {
+    const playIcon = document.getElementById("play");
+
+    if (playIcon.getAttribute("src") == "./stop.svg") {
+        activeSynth.forEach(synth => synth.disconnect());
+        playIcon.setAttribute("src", "./play.svg");
+        clearTimeout(playTimeout);
+        return;
+    }
+
+    playIcon.setAttribute("src", "./stop.svg");
     activeSynth.forEach(synth => synth.disconnect());
+    let end = 0;
+
     const volume = document.getElementById("volume").value;
     const notes = getNotes();
+    const now = Tone.now();
+
     notes.forEach(note => {
         const synth = new Tone.Synth({ volume }).toDestination();
-        const now = Tone.now();
         synth.triggerAttackRelease(note.freq, note.dur, now + note.start);
         activeSynth.push(synth);
+
+        if (note.start + note.dur > end) {
+            end = note.start + note.dur;
+        }
     });
+
+    playTimeout = setTimeout(() => {
+        playIcon.setAttribute("src", "./play.svg");
+    }, end * 1000);
 }
